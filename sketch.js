55bf09485e84395a326ac0e96fb8711d993bf087
Anvith653 <75430571+Anvith653@users.noreply.github.com>
2021-02-06 09:06:03 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function preload(){
+    
+}
+
+function setup(){
+   
+    
+}
+
+function draw(){
+    var maxDrops = 100
+
+    for(var i=0; i<maxDrops; i++){
+        drop.push(new createDrop(random(0,400), (random(0,400)));
+}   
+
